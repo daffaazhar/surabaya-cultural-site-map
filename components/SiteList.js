86c0ccb84c8be16d72f5ca02feed6b0b2ca5a542
daffaazhar <daffaazharsda@gmail.com>
2024-05-23 14:51:05 +0700

    feat: add search features

--- components/SiteList.js
+++ components/SiteList.js
@@ -1,7 +1,12 @@
-import { dayMap } from "@/utils/dayMap";
 import Image from "next/image";
+import { dayMap } from "@/utils/dayMap";
+import { useEffect, useRef, useState } from "react";
+import { Input } from "./ui/input";
+import "@lottiefiles/lottie-player";
 
 const SiteList = ({ sites, onSelect }) => {
+  const [searchQuery, setSearchQuery] = useState("");
+
   const daysOfWeek = [
     "Sunday",
     "Monday",
@@ -42,6 +47,13 @@ const SiteList = ({ sites, onSelect }) => {
     return null;
   };
 
+  const filteredSites = sites.filter(
+    (site) =>
+      site.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      site.description.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      site.address.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <div className="p-4">
       <div className="flex items-center mb-4 gap-x-4">
@@ -53,50 +65,66 @@ const SiteList = ({ sites, onSelect }) => {
         />
         <h2 className="text-2xl font-bold">Situs Budaya Surabaya</h2>
       </div>
-      <p className="text-sm text-gray-600 mb-4">
-        Menampilkan: {sites.length} hasil
+      <Input
+        type="text"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        placeholder="Cari situs budaya disini..."
+        className="focus:outline focus:outline-0 focus:ring-0 focus:ring-offset-0 mb-4"
+      />
+      <p className="text-sm text-gray-600 mb-3">
+        Menampilkan: {filteredSites.length} hasil
       </p>
-      {sites.map((site, index) => {
-        const { isOpen, closingTime } = checkIfOpen(site.openingHours);
-        const nextOpeningTime = !isOpen
-          ? getNextOpeningTime(site.openingHours)
-          : null;
+      {filteredSites.length == 0 ? (
+        <div className="flex flex-col justify-center items-center mt-28">
+          <h3 className="font-semibold text-xl">
+            Situs Budaya Tidak Ditemukan
+          </h3>
+          <p className="text-sm mt-2">Coba gunakan kata kunci lainnya!</p>
+        </div>
+      ) : (
+        filteredSites.map((site, index) => {
+          const { isOpen, closingTime } = checkIfOpen(site.openingHours);
+          const nextOpeningTime = !isOpen
+            ? getNextOpeningTime(site.openingHours)
+            : null;
 
-        return (
-          <div
-            key={index}
-            className="mb-4 p-4 border rounded-lg cursor-pointer hover:bg-gray-100 transition-all duration-300"
-            onClick={() => onSelect(site)}
-          >
-            {" "}
-            <h3 className="text-xl font-semibold mb-2">{site.name}</h3>
-            <p className="text-sm text-gray-600 mb-0.5">{site.address}</p>
-            {site.tel != null && (
-              <p className="text-sm text-gray-600 mb-0.5">{site.tel}</p>
-            )}
-            <div className="flex items-center gap-x-1">
-              {isOpen ? (
-                <span className="text-sm text-green-600">
-                  {" "}
-                  Buka <span className="text-[8px]">•</span>{" "}
-                  <span className="text-sm text-gray-600">
-                    Hingga pukul {closingTime} WIB
+          return (
+            <div
+              key={index}
+              className="mb-4 p-4 border rounded-lg cursor-pointer hover:bg-gray-100 transition-all duration-300"
+              onClick={() => onSelect(site)}
+            >
+              {" "}
+              <h3 className="text-xl font-semibold mb-2">{site.name}</h3>
+              <p className="text-sm text-gray-600 mb-0.5">{site.address}</p>
+              {site.tel != null && (
+                <p className="text-sm text-gray-600 mb-0.5">{site.tel}</p>
+              )}
+              <div className="flex items-center gap-x-1">
+                {isOpen ? (
+                  <span className="text-sm text-green-600">
+                    {" "}
+                    Buka <span className="text-[8px]">•</span>{" "}
+                    <span className="text-sm text-gray-600">
+                      Hingga pukul {closingTime} WIB
+                    </span>
                   </span>
-                </span>
-              ) : (
-                <span className="text-sm text-red-500">
-                  {" "}
-                  Tutup<span className="text-[8px]">•</span>{" "}
-                  <span className="text-sm text-gray-600">
-                    Buka pada {nextOpeningTime.day} pukul{" "}
-                    {nextOpeningTime.hours} WIB
+                ) : (
+                  <span className="text-sm text-red-500">
+                    {" "}
+                    Tutup<span className="text-[8px]">•</span>{" "}
+                    <span className="text-sm text-gray-600">
+                      Buka pada {nextOpeningTime.day} pukul{" "}
+                      {nextOpeningTime.hours} WIB
+                    </span>
                   </span>
-                </span>
-              )}{" "}
+                )}{" "}
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })
+      )}
     </div>
   );
 };
